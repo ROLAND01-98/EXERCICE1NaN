4dbd4e705a4c89d1193e5e05fea6cd7178176139
ROLAND01-98 <77779643+ROLAND01-98@users.noreply.github.com>
2021-03-26 11:27:29 +0000

    Add files via upload

---
+++ exercice1nan.py
@@ -0,0 +1,12 @@
+from random import randint
+
+nbre = randint(1,100)
+devine_nbre = ()
+while devine_nbre != nbre :
+    devine_nbre = int(input("Devinez un nombre svp : "))
+    if devine_nbre < nbre:
+        print("Ce nombre est trop petit !")
+    elif devine_nbre > nbre:
+        print("Ce nombre est trop grand !")
+    else:
+        print("Bravo vous avez touvez le nombre !")
